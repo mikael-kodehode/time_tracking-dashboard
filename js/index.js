9507029e7a43360f924cea554a750312944f5f6c
mikael-kodehode <93524362+mikael-kodehode@users.noreply.github.com>
2022-01-24 20:25:18 +0100

    Cleaned up

--- js/index.js
+++ js/index.js
@@ -103,121 +103,130 @@ data = [
     }
   ]
 // Timeframe buttons
-const dailyBtn = document.querySelector("#daily")
-const weeklyBtn = document.querySelector("#weekly")
-const monthlyBtn = document.querySelector("#monthly")
+const dailyBtn = document.querySelector("#daily");
+const weeklyBtn = document.querySelector("#weekly");
+const monthlyBtn = document.querySelector("#monthly");
 
 //Main elements showing hours
-const workHours = document.querySelector("#work-hours")
-const playHours = document.querySelector("#play-hours")
-const studyHours = document.querySelector("#study-hours")
-const excerciseHours = document.querySelector("#excercise-hours")
-const socialHours = document.querySelector("#social-hours")
-const selfCareHours = document.querySelector("#self-care-hours")
+const workHours = document.querySelector("#work-hours");
+const playHours = document.querySelector("#play-hours");
+const studyHours = document.querySelector("#study-hours");
+const excerciseHours = document.querySelector("#excercise-hours");
+const socialHours = document.querySelector("#social-hours");
+const selfCareHours = document.querySelector("#self-care-hours");
 //Creating an array for easy use
-const hoursArr = [workHours, playHours, studyHours, excerciseHours, socialHours, selfCareHours]
+const hoursArr = [workHours, playHours, studyHours, excerciseHours, socialHours, selfCareHours];
 
 //Elements showing hours last day/week/month
-const workLast = document.querySelector("#work-last")
-const playLast = document.querySelector("#play-last")
-const studyLast = document.querySelector("#study-last")
-const excerciseLast = document.querySelector("#excercise-last")
-const socialLast = document.querySelector("#social-last")
-const selfCareLast = document.querySelector("#self-care-last")
+const workLast = document.querySelector("#work-last");
+const playLast = document.querySelector("#play-last");
+const studyLast = document.querySelector("#study-last");
+const excerciseLast = document.querySelector("#excercise-last");
+const socialLast = document.querySelector("#social-last");
+const selfCareLast = document.querySelector("#self-care-last");
 //Same, creating an array for easy use
-const lastArr = [workLast, playLast, studyLast, excerciseLast, socialLast, selfCareLast]
+const lastArr = [workLast, playLast, studyLast, excerciseLast, socialLast, selfCareLast];
 
-function addOpacity() {
-    dailyBtn.classList.remove("active-timeframe")
-    weeklyBtn.classList.remove("active-timeframe")
-    monthlyBtn.classList.remove("active-timeframe")
-    dailyBtn.classList.add("opacity")
-    weeklyBtn.classList.add("opacity")
-    monthlyBtn.classList.add("opacity")
-}
-
-function showDaily() {
-    for (x in hoursArr) 
-    {        
-        if (data[x].timeframes.daily.current === 1) 
-        {
-            hoursArr[x].innerHTML = "1hr"
-        }
-        else 
-        {
-            hoursArr[x].innerHTML = `${data[x].timeframes.daily.current}hrs`
-        }
-        if (data[x].timeframes.daily.previous === 1) 
-        {
-            lastArr[x].innerHTML = `Last day - ${data[x].timeframes.daily.previous}hr`
-        }
-        else
-        {
-            lastArr[x].innerHTML = `Last day - ${data[x].timeframes.daily.previous}hrs`
-        }
-    }
-}
-function showWeekly() 
+//Function that resets the active button
+function addOpacity() 
 {
-    for (x in hoursArr) 
-    {        
-        if (data[x].timeframes.weekly.current === 1) 
-        {
-            hoursArr[x].innerHTML = "1hr"
-        }
-        else 
-        {
-            hoursArr[x].innerHTML = `${data[x].timeframes.weekly.current}hrs`
-        }
-        if (data[x].timeframes.weekly.previous === 1) 
-        {
-            lastArr[x].innerHTML = `Last day - ${data[x].timeframes.weekly.previous}hr`
-        }
-        else
-        {
-            lastArr[x].innerHTML = `Last day - ${data[x].timeframes.weekly.previous}hrs`
-        }
+    const timeframe = [dailyBtn, weeklyBtn, monthlyBtn]
+    for (let x = 0; x < timeframe.length; x++) 
+    {
+        timeframe[x].classList.remove("active-timeframe");
+        timeframe[x].classList.add("opacity");
     }
 }
 
-function showMonthly() 
+//A switch based on a specific parameter from each event listener deciding what timeframe to show.
+function showHours(number) 
 {
-    for (x in hoursArr) 
-    {        
-        if (data[x].timeframes.monthly.current === 1) 
-        {
-            hoursArr[x].innerHTML = "1hr"
-        }
-        else 
-        {
-            hoursArr[x].innerHTML = `${data[x].timeframes.monthly.current}hrs`
-        }
-        if (data[x].timeframes.monthly.previous === 1) 
-        {
-            lastArr[x].innerHTML = `Last day - ${data[x].timeframes.monthly.previous}hr`
-        }
-        else
-        {
-            lastArr[x].innerHTML = `Last day - ${data[x].timeframes.monthly.previous}hrs`
-        }
+    addOpacity()
+    switch (number)
+    {
+        case 1:
+            dailyBtn.classList.remove("opacity");
+            dailyBtn.classList.add("active-timeframe");
+            for (x in hoursArr) 
+            {        
+                if (data[x].timeframes.daily.current === 1) 
+                {
+                    hoursArr[x].innerHTML = "1hr";
+                }
+                else 
+                {
+                    hoursArr[x].innerHTML = `${data[x].timeframes.daily.current}hrs`;
+                }
+                if (data[x].timeframes.daily.previous === 1) 
+                {
+                    lastArr[x].innerHTML = `Last day - ${data[x].timeframes.daily.previous}hr`;
+                }
+                else
+                {
+                    lastArr[x].innerHTML = `Last day - ${data[x].timeframes.daily.previous}hrs`;
+                }
+            }
+            break
+        case 2:
+            weeklyBtn.classList.remove("opacity");
+            weeklyBtn.classList.add("active-timeframe");
+            for (x in hoursArr) 
+            {        
+                if (data[x].timeframes.weekly.current === 1) 
+                {
+                    hoursArr[x].innerHTML = "1hr";
+                }
+                else 
+                {
+                    hoursArr[x].innerHTML = `${data[x].timeframes.weekly.current}hrs`;
+                }
+                if (data[x].timeframes.weekly.previous === 1) 
+                {
+                    lastArr[x].innerHTML = `Last day - ${data[x].timeframes.weekly.previous}hr`;
+                }
+                else
+                {
+                    lastArr[x].innerHTML = `Last day - ${data[x].timeframes.weekly.previous}hrs`;
+                }
+            }
+            break
+        case 3:
+            monthlyBtn.classList.remove("opacity");
+            monthlyBtn.classList.add("active-timeframe");
+            for (x in hoursArr) 
+            {        
+                if (data[x].timeframes.monthly.current === 1) 
+                {
+                    hoursArr[x].innerHTML = "1hr";
+                }
+                else 
+                {
+                    hoursArr[x].innerHTML = `${data[x].timeframes.monthly.current}hrs`;
+                }
+                if (data[x].timeframes.monthly.previous === 1) 
+                {
+                    lastArr[x].innerHTML = `Last day - ${data[x].timeframes.monthly.previous}hr`;
+                }
+                else
+                {
+                    lastArr[x].innerHTML = `Last day - ${data[x].timeframes.monthly.previous}hrs`;
+                }
+            }
+            break
+        default:
+            console.log("Error. Invalid entry to switch.");
     }
 }
 
-dailyBtn.addEventListener("click", function(){
-    addOpacity()
-    dailyBtn.classList.remove("opacity")
-    dailyBtn.classList.add("active-timeframe")
-    showDaily()
+dailyBtn.addEventListener("click", function() 
+{
+    showHours(1);
 })
-weeklyBtn.addEventListener("click", function() {
-    addOpacity()
-    weeklyBtn.classList.remove("opacity")
-    weeklyBtn.classList.add("active-timeframe")
-    showWeekly()
+weeklyBtn.addEventListener("click", function()
+{
+    showHours(2);
 })
-monthlyBtn.addEventListener("click", function() {
-    addOpacity()
-    monthlyBtn.classList.remove("opacity")
-    monthlyBtn.classList.add("active-timeframe")
-    showMonthly()
+monthlyBtn.addEventListener("click", function() 
+{
+    showHours(3);
 })
